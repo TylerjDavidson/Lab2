fe5d6f8fe2642065d9930a0fff5db26abad67f33
Carly <cxdonner@gmail.com>
2018-02-04 23:15:25 -0500

    more edits

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ import Launcher
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((500,400)) #window size
+DISPLAYSURF = pygame.display.set_mode((500,400)) #main window
 pygame.display.set_caption("Launchr") #window title
 #s = serial.Serial("/dev/ttyACM0") #uncomment eventually
 FPS = 30 #frames/s
@@ -24,21 +24,27 @@ def draw_world(surf): #background function
   textRectObj.center = (250,20)
   pygame.draw.rect(surf,(20,150,200),(0,0,500,380)) #sky
   pygame.draw.rect(surf,(0,255,20),(0,380,500,20)) #grass
+  (surf).blit(textSurfaceObj,textRectObj) #draw title text
 
-
-while True: #main loop
+while(True): #main loop
   draw_world(DISPLAYSURF) #draw background
-  DISPLAYSURF.blit(textSurfaceObj,textRectObj) #draw title text
+  
 
 
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_UP:
+      if event.key == pygame.K_UP: #rotate up
         Launcher.changeAngle(3)
+      if event.key == pygame.K_DOWN: #rotate down
+        Launcher.changeAngle(-3)
+      if event.key == pygame.K_LEFT: #decrease power
+        Launcher.changeMagnitude(-3)
+      if event.key == pygame.K_RIGHT: #increase power
+        Launcher.changeMagnitude(3)
     if event.type == QUIT:
       pygame.quit()
       sys.exit()
-  Launcher.draw(DISPLAYSURF) #display Launcher  
+  Launcher.drawCanon(DISPLAYSURF) #display Launcher  
   pygame.display.update()
   fpsClock.tick(FPS)
 
