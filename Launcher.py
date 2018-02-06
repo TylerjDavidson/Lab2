76ef8f27a808f91b838e7252d40a67e5e380fbe0
Carly <cxdonner@gmail.com>
2018-02-05 20:27:18 -0500

    fixed up a lot of things

--- Launcher.py
+++ Launcher.py
@@ -8,32 +8,32 @@ MIN_ANGLE = 0
 
 
 class launcher:
-  
-  def __init__(self,x,y):
-     self.x = x
-     self.y = y
-     self.magnitude = 50
-     self.angle = 45
-   
-  def changeMagnitude(self,delta):
-    if delta <= 100 and delta >= 10:
-      self.magnitude = self.magnitude + self.delta 
-    else: 
-      delta = 0
-      self.magnitude = self.magnitude + self.delta
 
-  def changeAngle(self,dtheta):
-    if dtheta <= 90 and dtheta >= 0:
-      self.angle = self.angle + self.dtheta
-    else:
-      dtheta = 0
-      self.angle = self.angle + self.dtheta
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.magnitude = 50
+		self.angle = 45
 
-  def drawCannon(self,surf):
-    X1 = x + self.magnitude*cos(self.dtheta)
-    Y1 = y + self.magnitude*sin(self.dtheta)
-      
-         
+	def changeMagnitude(self,delta):
+		if delta <= 100 and delta >= 10:
+			self.magnitude = self.magnitude + delta 
+		else: 
+			delta = 0
+			self.magnitude = self.magnitude + delta
+
+	def changeAngle(self,dtheta):
+		if dtheta <= 90 and dtheta >= 0:
+			self.angle = self.angle + dtheta
+		else:
+			dtheta = 0
+			self.angle = self.angle + dtheta
+
+	def drawCannon(self,surf):
+		X1 = self.x + self.magnitude*math.cos(self.angle) 
+		Y1 = self.y + self.magnitude*math.sin(self.angle)
+
+
 
 #Starting at (0,380)
 #r = 10
--- game.py
+++ game.py
@@ -6,47 +6,50 @@ from pygame.locals import *
 import math
 import Launcher
 
-def main():
-  pygame.init()
-  DISPLAYSURF = pygame.display.set_mode((500,400)) #main window
-  pygame.display.set_caption("Launcher") #window title
-  #s = serial.Serial("/dev/ttyACM0") #uncomment eventually
-  FPS = 30 #frames/s
-  fpsClock = pygame.time.Clock()
-  Launcher1 = Launcher.launcher(0,380)
+def draw_world(surf):
+	fontObj = pygame.font.Font('freesansbold.ttf',32) #font and font size
+	textSurfaceObj = fontObj.render('Launcher 1.0',True,(255,255,255),(0,0,0)) #white text, black highlight
+	textRectObj = textSurfaceObj.get_rect()
+	textRectObj.center = (250,20)
+	pygame.draw.rect(surf,(20,150,200),(0,0,500,380)) #sky
+	pygame.draw.rect(surf,(0,255,20),(0,380,500,20)) #grass
+	(surf).blit(textSurfaceObj,textRectObj) #draw title text
 
-  def draw_world(surf): #background function
-    fontObj = pygame.font.Font('freesansbold.ttf',32) #font and font size
-    textSurfaceObj = fontObj.render('Launcher 1.0',True,(255,255,255),(0,0,0)) #white text, black highlight
-    textRectObj = textSurfaceObj.get_rect()
-    textRectObj.center = (250,20)
-    pygame.draw.rect(surf,(20,150,200),(0,0,500,380)) #sky
-    pygame.draw.rect(surf,(0,255,20),(0,380,500,20)) #grass
-    (surf).blit(textSurfaceObj,textRectObj) #draw title text
+pygame.init() 
+DISPLAYSURF = pygame.display.set_mode((500,400)) #main window
+#s = serial.Serial("/dev/ttyACM0") #uncomment eventually
+FPS = 30 #frames/s
+fpsClock = pygame.time.Clock()
+Launcher1 = Launcher.launcher(0,380)
 
-  while(True): #main loop
-    draw_world(DISPLAYSURF) #draw background
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_UP: #rotate up
-          Launcher.changeAngle(3)
-        if event.key == pygame.K_DOWN: #rotate down
-          Launcher.changeAngle(-3)
-        if event.key == pygame.K_LEFT: #decrease power
-          Launcher.changeMagnitude(-5)
-        if event.key == pygame.K_RIGHT: #increase power
-          Launcher.changeMagnitude(5)
-      if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
-    Launcher1.drawCannon(DISPLAYSURF) #display Launcher  
-    pygame.display.update()
-    fpsClock.tick(FPS)
+while(True):
+	draw_world(DISPLAYSURF) #draw background
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP: 
+				#rotate up
+				Launcher1.changeAngle(3)
+			if event.key == pygame.K_DOWN: 
+				#rotate down
+				Launcher1.changeAngle(-3)
+			if event.key == pygame.K_LEFT:
+				#decrease power
+				Launcher1.changeMagnitude(-5)
+			if event.key == pygame.K_RIGHT:
+				#increase power
+				Launcher1.changeMagnitude(5)
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	Launcher1.drawCannon(DISPLAYSURF) #display Launcher  
+	pygame.display.update()
+	fpsClock.tick(FPS)
 
 
 
-      
-         
+
+
 
 
 
